12663b6f1a57214e15a3738ed973a6596f78d66a
Valastiri <reyes.nico@gmail.com>
2022-08-09 15:23:23 +0800

    update about section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,8 +6,8 @@ export default function About() {
         <main className={styles.main}>
             <h2 className={styles.title}>About</h2>
             <p className={styles.content}>
-                I originally joined the FlyByWire organization as part of the Media Team.
-                Alongside marketing, I also now manage our supporting documentation
+                I was originally invited to join the FlyByWire organization as part of the Media Team back in September 2020.
+                Alongside marketing, I also now manage the FlyByWire documentation website
                 to assist our end-users and provide a knowledge base for all flight sim enthusiasts.
             </p>
         </main>
